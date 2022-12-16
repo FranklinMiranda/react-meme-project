06a75480ff6e14743fe7d7287a76199856ddc1f9
FranklinMiranda <fdm61344@gmail.com>
2022-12-16 11:31:41 -0500

    CM

--- react-meme-project/src/Component/Button.js
+++ react-meme-project/src/Component/Button.js
@@ -4,7 +4,7 @@ class Button extends React.Component {
     render () {
         return (
             <div className='Button'>
-                
+                <input type='button'>Generate New Meme</input>
             </div>
         )
     }
