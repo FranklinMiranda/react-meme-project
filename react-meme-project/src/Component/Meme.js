a2336785056e1ce5f74a0be42679f7b2ac27ccb4
FranklinMiranda <fdm61344@gmail.com>
2022-12-16 15:23:05 -0500

    CM

--- react-meme-project/src/Component/Meme.js
+++ react-meme-project/src/Component/Meme.js
@@ -1,14 +1,27 @@
 import React from 'react';
 
+// There are two differnet ways to add event listeners to a Web App
+// The first way is using JavaScript .addEventListener('click', function () {})
+// The second way is using the onClick attribute in HTML
+// In React we used onClick and can directly write a function within {}
+
 class Meme extends React.Component {
   render() {
+    function handleClick() {
+      console.log('I was clicked!');
+    }
     return (
       <div className="Meme-Div">
         <form className="Meme-Form">
-          <input type="text" className="Meme-Input" placeholder='Top Text'/>
-          <input type="text" className="Meme-Input" placeholder='Bottom Text'/>
-          <button className="Meme-Button">Get a new meme image</button>
+          <input type="text" className="Meme-Input" placeholder="Top Text" />
+          <input type="text" className="Meme-Input" placeholder="Bottom Text" />
+          <button className="Meme-Button" onClick={handleClick}>
+            Get a new meme image
+          </button>
         </form>
+        <div className="Image-Div">
+          <img></img>
+        </div>
       </div>
     );
   }
