dbffbae5f703adeb4030b20d294c4ba044d7dbd9
FranklinMiranda <fdm61344@gmail.com>
2022-12-16 11:10:58 -0500

    CM

---
+++ react-meme-project/src/Component/Input.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Input extends React.Component {
+  render(props) {
+    return (
+      <div className="Input">
+        <h3>{this.props.label}</h3>
+        <input type="text" />
+      </div>
+    );
+  }
+}
