2db194cda6c0a7482eb5414496fe254c71780191
FranklinMiranda <fdm61344@gmail.com>
2022-12-16 14:09:47 -0500

    CM

--- react-meme-project/src/Component/Meme.js
+++ react-meme-project/src/Component/Meme.js
@@ -2,6 +2,12 @@ import React from 'react';
 
 class Meme extends React.Component {
   render() {
-    return <div className="Meme-Div"></div>;
+    return <div className="Meme-Div">
+        <form>
+            <input type='text'/>
+            <input type='text'/>
+            <button>Get a new meme image</button>
+        </form>
+    </div>;
   }
 }
