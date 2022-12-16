a2801506fe8b4c43c20b6d5c0cb0b2d61a9c8d88
FranklinMiranda <fdm61344@gmail.com>
2022-12-16 14:07:21 -0500

    CM

--- react-meme-project/src/Component/App.js
+++ react-meme-project/src/Component/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './Header';
 import Form from './Form';
-import Image from './Image';
+import Image from './Meme';
 
 class App extends React.Component {
   render() {
