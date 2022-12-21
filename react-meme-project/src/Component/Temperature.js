9a017dfe6262210f933652b4ae541021a864eb4a
FranklinMiranda <fdm61344@gmail.com>
2022-12-21 16:44:09 -0500

    CM

--- react-meme-project/src/Component/Temperature.js
+++ react-meme-project/src/Component/Temperature.js
@@ -7,3 +7,29 @@ function BoilingVerdict(props) {
     return <p>The water would not boil.</p>
 }
 
+class Calculator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {temperature: ''};
+
+        this.handleChange = this.handleChange.bind(this)
+
+    }
+
+    handleChange(e) {
+        this.setState({temperature: e.target.value});
+    }
+
+    render(){
+        const temperature = this.state.temperature;
+        return (
+            <fieldset>
+                <legend>Enter temperature in Celsius:</legend>
+                <input value={temperature} onChange={this.handleChange}/>
+                <BoilingVerdict celsius={parseFloat(temperature)} />
+            </fieldset>
+        )
+    }
+
+
+}
