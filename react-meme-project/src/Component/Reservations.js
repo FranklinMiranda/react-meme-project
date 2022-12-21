d10195d686d5945675b4136ecd76cb650ee09eef
FranklinMiranda <fdm61344@gmail.com>
2022-12-21 15:23:08 -0500

    CM

--- react-meme-project/src/Component/Reservations.js
+++ react-meme-project/src/Component/Reservations.js
@@ -13,6 +13,8 @@ class Reservations extends React.Component {
     const target = event.target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
     const name = target.name;
+
+    this.setState({[name]: value});
   }
 
 
