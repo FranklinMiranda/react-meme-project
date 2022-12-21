6ae96536ebff684d795cf3037c27bb51c96eb949
FranklinMiranda <fdm61344@gmail.com>
2022-12-21 17:17:25 -0500

    CM

--- react-meme-project/src/Component/Temperature.js
+++ react-meme-project/src/Component/Temperature.js
@@ -1,35 +1,32 @@
-import React from "react";
+import React from 'react';
 
 function BoilingVerdict(props) {
-    if (props.celsius >= 100) {
-        return <p>The water would boil.</p>;
-    }
-    return <p>The water would not boil.</p>
+  if (props.celsius >= 100) {
+    return <p>The water would boil.</p>;
+  }
+  return <p>The water would not boil.</p>;
 }
 
 class Calculator extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {temperature: ''};
+  constructor(props) {
+    super(props);
+    this.state = { temperature: '' };
 
-        this.handleChange = this.handleChange.bind(this)
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-    }
+  handleChange(e) {
+    this.setState({ temperature: e.target.value });
+  }
 
-    handleChange(e) {
-        this.setState({temperature: e.target.value});
-    }
-
-    render(){
-        const temperature = this.state.temperature;
-        return (
-            <fieldset>
-                <legend>Enter temperature in Celsius:</legend>
-                <input value={temperature} onChange={this.handleChange}/>
-                <BoilingVerdict celsius={parseFloat(temperature)} />
-            </fieldset>
-        )
-    }
-
-
+  render() {
+    const temperature = this.state.temperature;
+    return (
+      <fieldset>
+        <legend>Enter temperature in Celsius:</legend>
+        <input value={temperature} onChange={this.handleChange} />
+        <BoilingVerdict celsius={parseFloat(temperature)} />
+      </fieldset>
+    );
+  }
 }
